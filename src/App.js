abbccf062213673f8154897a9259a6b390c49f6b
grimhamr <rossiadisj@gmail.com>
2022-03-23 13:53:30 +0200

    build boxes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
-
+import Box from './Box';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,14 +22,25 @@ class App extends React.Component {
     });
   }
   render() {
+    let boxList = []
+    let boxCount = 4
+    for (let i = 0; i < boxCount; i++) {
+      let newBox = <Box label={i + 1} />
+      boxList = boxList.concat(newBox)
 
+    }
     return (
       <div id="container">
         <Navbar toggleSidebar={this.toggleSidebar} />
         <div id="main-page-container">
           {this.state.isSidebarVisible && <Sidebar />}
           <div id="page-contents">
+            <div id='boxes-container'>
+              {boxList}
+            </div>
+            <div id="data-lists-container">
 
+            </div>
           </div>
         </div>
       </div>

26260f0dcb5a0881a4bd3461d51d7b3df23b8e7b
grimhamr <rossiadisj@gmail.com>
2022-03-28 14:41:27 +0300

    fix sidebar style on mobile view

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,7 +3,12 @@ import './Sidebar.css';
 function Sidebar() {
     return (
         <div id="sidebar">
-
+            <ul id='sidebar-list'>
+                <li>Users </li>
+                <li>Settings</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
 
         </div>
     );
